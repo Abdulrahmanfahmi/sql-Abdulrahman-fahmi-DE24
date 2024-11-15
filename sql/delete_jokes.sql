ea2d77f3ba556d8db0d8de8c101478f7f3742e7e
abdulrahmanfahmi <abdirahman3016@gmail.com>
2024-11-15 14:27:40 +0100

    restructured files

---
+++ sql/delete_jokes.sql
@@ -0,0 +1,6 @@
+SELECT * FROM funny_jokes WHERE rating < 5;
+
+DELETE funny_jokes WHERE rating < 5;
+
+SELECT * FROM funny_jokes;
+ 
---
+++ sql/uppdate_jokes.sql
@@ -0,0 +1,5 @@
+SELECT * FROM funny_jokes;
+
+SELECT * FROM funny_jokes WHERE id = 2;
+
+UPDATE funny_jokes SET rating = 10 WHERE id = 2; 
